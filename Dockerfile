8f0a981b1c9177c8e4e53c9e5877ee72c6b4698d
Dawid Wijata <dawid.wijata@hotmail.com>
2022-11-05 17:31:25 +0100

    Solve CORS problems when connecting from container

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# stage 1
+
+FROM node:alpine AS build
+WORKDIR /app
+COPY . .
+RUN npm ci && npm run build
+
+# stage 2
+
+FROM nginx:alpine
+COPY --from=build /app/nginx/nginx.conf /etc/nginx/nginx.conf
+COPY --from=build /app/nginx/default.conf /etc/nginx/conf.d/default.conf
+COPY --from=build /app/dist/app /usr/share/nginx/html
+EXPOSE 80
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmptyRouteComponent } from "./empty-route/empty-route.component";
 
 const routes: Routes = [];
 
 @NgModule({
+  declarations: [EmptyRouteComponent],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
