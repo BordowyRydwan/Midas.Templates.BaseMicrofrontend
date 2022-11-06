e9330c0f667ca6c275026e62caef70a2128ed185
BordowyRydwan <s205006@sggw.edu.pl>
2022-11-06 10:53:52 +0100

    Add sample NSwag service

--- src/app/services/base-api.ts
+++ src/app/services/base-api.ts
@@ -1,3 +1,8 @@
+import { mergeMap as _observableMergeMap, catchError as _observableCatch } from 'rxjs/operators';
+import { Observable, from as _observableFrom, throwError as _observableThrow, of as _observableOf } from 'rxjs';
+import { Injectable, Inject, Optional, InjectionToken } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse, HttpResponseBase } from '@angular/common/http';
+
 export default class BaseApiService {
   protected transformOptions(options: any): Promise<any> {
     return Promise.resolve(options);
